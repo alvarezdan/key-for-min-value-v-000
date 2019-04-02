3e0b165482f2b0173e27944fd04a6786b04b3448
Daniel Alvarez <dnielalvrz@gmail.com>
2019-04-02 17:38:23 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,10 +4,10 @@
 def key_for_min_value(name_hash)
     min_value = nil
     min_key = nil
-    name_hash.each do |key, value|
-      if value == nil || value < min_value
-        min_value = value
-        min_key = key
+    name_hash.each do |k, v|
+      if value == nil || v < min_value
+        min_value = v
+        min_key = k
       end
     end
     min_key
